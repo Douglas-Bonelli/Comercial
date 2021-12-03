49527f10df00f28d03f63d7788889a5e835ec49b
Douglas-Bonelli <doulas.bonelli@gmail.com>
2021-12-03 06:14:54 -0300

    First Commit

---
+++ src/main/java/br/com/intelligenceweb/comercial/entity/EnderecoTipo.java
@@ -0,0 +1,15 @@
+package br.com.intelligenceweb.comercial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoTipo {
+	
+	private Long id;
+	private String descricao;
+
+}
---
+++ src/test/java/br/com/intelligenceweb/comercial/ComercialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intelligenceweb.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
